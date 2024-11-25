e17f8f1791b451a42c0296c95bf89428f723e74c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-25 18:46:29 +0600

    feat: add toggle icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,14 @@
 <template>
-    <nav>
+    <nav class="flex items-center justify-between p-4 bg-primary-200">
         <p>Logo</p>
-        <Button @click="next()">{{ mode }}</Button>    
+
+        <div class="flex items-center justify-between">
+            <Button @click="next()">
+                <SunDim :size="20" v-if="mode === 'dark'" />
+                <Moon :size="20" v-if="mode === 'light'" />
+                <MonitorDot :size="20" v-if="mode === 'auto'" />{{ mode }}
+            </Button>
+        </div>
     </nav>
 </template>
 
@@ -9,13 +16,14 @@
 import { useColorMode, useCycleList } from '@vueuse/core'
 import { Button } from 'primevue';
 import { watchEffect } from 'vue'
+import { SunDim, Moon, MonitorDot } from 'lucide-vue-next';
 
 const mode = useColorMode({
-  emitAuto: true,
-  modes: {
-    contrast: 'dark contrast',
-    cafe: 'cafe',
-  },
+    emitAuto: true,
+    modes: {
+        contrast: 'dark contrast',
+        cafe: 'cafe',
+    },
 })
 
 const { state, next } = useCycleList(['dark', 'light', 'auto'] as const, { initialValue: mode })
