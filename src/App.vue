fb3ab2612935346a2869db396e82419d97bd09dc
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-25 18:50:48 +0600

    style: set color for dark

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="dark:bg-surface-900 dark:text-surface-200">
-    <Navbar />
-    <nav>
-      <RouterLink to="/">Go to Home</RouterLink>
-      <RouterLink to="/about">Go to About</RouterLink>
-    </nav>
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <Navbar />
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <script setup>
