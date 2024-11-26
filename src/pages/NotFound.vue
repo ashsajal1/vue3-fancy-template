ed0108132fa1974efb15feaa68afdbb4216a1759
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-26 07:48:27 +0600

    feat: add home btn

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,4 +1,14 @@
 <template>
-    <h1 class="text-4xl font-extrabold">404</h1>
-    <h1>Page not found</h1>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-extrabold">404</h1>
+        <h1>Page not found</h1>
+
+        <RouterLink to="/">
+            <Button class='mt-2'>Go Home</Button>
+        </RouterLink>
+    </div>
 </template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
