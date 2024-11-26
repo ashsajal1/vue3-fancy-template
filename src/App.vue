27a184d215d07038f3ebcf7dba2ccd3bffe29de3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-26 07:49:00 +0600

    feat: remove nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <Navbar />
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
-  </nav>
   <main>
     <RouterView />
   </main>
