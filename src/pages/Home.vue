069b8e40abc6fb3f712bf98c7719933230ad8f54
Ashsajal <ashsajal1@gmail.com>
2025-04-08 21:08:50 +0600

    feat: use count store

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,14 @@
 <template>
-    <h1>This is home page</h1>
+  <h1>Count is {{ store.count }}</h1>
+
+  <div class="flex gap-2">
+    <Button label="Increment" @click="store.increment" />
+  </div>
 </template>
 
 <script setup>
+import { useCounterStore } from "../stores/counter";
+import { Button } from "primevue";
+
+const store = useCounterStore();
 </script>
