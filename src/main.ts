fb743f48108194bb72b8181f219b11a8c526420b
Ashsajal <ashsajal1@gmail.com>
2025-04-08 20:57:27 +0600

    feat: add pinia plugin

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import "./assets/tailwind.css";
 import "./style.css";
-import App from './App.vue'
-import { router } from './router'
-import { MotionPlugin } from '@vueuse/motion'
-import PrimeVue from 'primevue/config';
+import App from "./App.vue";
+import { router } from "./router";
+import { MotionPlugin } from "@vueuse/motion";
+import PrimeVue from "primevue/config";
+import { createPinia } from "pinia";
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
 app.use(MotionPlugin);
 app.use(PrimeVue, {
-    theme: 'none'
+  theme: "none",
 });
+app.use(pinia);
 
-app.mount('#app');
+app.mount("#app");
